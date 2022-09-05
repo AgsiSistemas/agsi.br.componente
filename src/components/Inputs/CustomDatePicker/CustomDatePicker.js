999967005cf59720c94de6f2e4c1cf6f1539ce79
Giovane Sedano <35101491+giovaness30@users.noreply.github.com>
2022-09-05 09:14:27 -0300

    ...

--- src/components/Inputs/CustomDatePicker/CustomDatePicker.js
+++ src/components/Inputs/CustomDatePicker/CustomDatePicker.js
@@ -6,26 +6,12 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { ptBR } from 'date-fns/locale'
 import { TextField } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const style = {
   date_picker: {
     padding: '9px 9px 9px 15px !important'
   }
 }
-const theme = createTheme({
-  components: {
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          input: {
-            padding: '8px',
-          }
-        },
-      },
-    },
-  },
-});
 
 const CustomDatePicker = ({ id, size, label, value, minDate, maxDate, dateFormat, onChange, placeHolder, helperText, disabled }) => {
 
@@ -33,36 +19,33 @@ const CustomDatePicker = ({ id, size, label, value, minDate, maxDate, dateFormat
 
   return (
     <React.Fragment>
-      <ThemeProvider theme={theme}>
-
-        <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptBR}>
-          <DatePicker
-            size={size}
-            label={label}
-            value={value}
-            disabled={disabled}
-            minDate={minDate ? new Date(minDate) : undefined}
-            maxDate={maxDate ? new Date(maxDate) : undefined}
-            dateFormat={dateFormat || "MM-DD-YYYY"}
-            onError={(e) => setDateValidation(e)}
-            onChange={onChange}
-            renderInput={params => (
-              <TextField
-                {...params}
-                inputProps={{
-                  ...params.inputProps,
-                  placeholder: placeHolder || "DD/MM/AAAA",
-                  // style: style.date_picker,
-                }}
-                id='custom-date-picker'
-                sx={{ minWidth: '100px', marginTop: '5px' }}
-                error={!value || dateValidation}
-                helperText={!value || dateValidation ? helperText || "Data ou Hora Invalida!" : ""}
-              />
-            )}
-          />
-        </LocalizationProvider>
-      </ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptBR}>
+        <DatePicker
+          size={size}
+          label={label}
+          value={value}
+          disabled={disabled}
+          minDate={minDate ? new Date(minDate) : undefined}
+          maxDate={maxDate ? new Date(maxDate) : undefined}
+          dateFormat={dateFormat || "MM-DD-YYYY"}
+          onError={(e) => setDateValidation(e)}
+          onChange={onChange}
+          renderInput={params => (
+            <TextField
+              {...params}
+              inputProps={{
+                ...params.inputProps,
+                placeholder: placeHolder || "DD/MM/AAAA",
+                style: style.date_picker,
+              }}
+              id='custom-date-picker'
+              sx={{ minWidth: '100px', marginTop: '5px' }}
+              error={!value || dateValidation}
+              helperText={!value || dateValidation ? helperText || "Data ou Hora Invalida!" : ""}
+            />
+          )}
+        />
+      </LocalizationProvider>
     </React.Fragment>
   )
 }
