055f683f6762577d45b9ebb31f65de8382b2be3d
Giovane Sedano <35101491+giovaness30@users.noreply.github.com>
2022-09-06 14:21:30 -0300

    ...

--- src/components/Inputs/CustomInputSelect/CustomInputSelect.js
+++ src/components/Inputs/CustomInputSelect/CustomInputSelect.js
@@ -1,98 +1,100 @@
 import React from 'react';
-import  PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import { CircularProgress } from '@mui/material'
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const isNullValue = (value) =>{
-    return ((value===undefined) || (value===null) || (value===""))    
+const isNullValue = (value) => {
+    return ((value === undefined) || (value === null) || (value === ""))
 }
 
-const handleHelperText = (validation) =>{
-    return validation? "Campo obrigatório": ""
+const handleHelperText = (validation) => {
+    return validation ? "Campo obrigatório" : ""
 }
 
-const handleError = (value, validation) =>{
+const handleError = (value, validation) => {
     return isNullValue(value) && validation
 }
 
 
-const CustomInputSelect = ({ title, options, value, freeSolo, onChange, inputValue, onInputChange, validation, loadingListOptions, open, disabled, onKeyPress, onblur }) =>{ 
+const CustomInputSelect = ({ title, options, value, freeSolo, onChange, inputValue, onInputChange, validation, loadingListOptions, open, disabled, onKeyPress, onblur, ...other }) => {
 
-    return(  
+    return (
         <React.Fragment>
             <Autocomplete
-                style={{marginTop:'8px'}}
+                style={{ marginTop: '8px' }}
                 size="small"
                 margin="dense"
-                variant="outlined"                
-                freeSolo={ freeSolo }            
-                InputLabelProps={{shrink: true}}  
-                fullWidth                
-                open={ open }           
-                disabled={ disabled }
-                options={ options }
-                loading={ options }
-                loadingText='Carregando...' 
-                onBlur={ onblur }               
-                value={ value }
-                onChange={ onChange }
-                inputValue={ inputValue }
-                onInputChange={ onInputChange }
-                onKeyPress={ onKeyPress }
+                variant="outlined"
+                freeSolo={freeSolo}
+                InputLabelProps={{ shrink: true }}
+                fullWidth
+                open={open}
+                disabled={disabled}
+                options={options}
+                loading={options}
+                loadingText='Carregando...'
+                onBlur={onblur}
+                value={value}
+                onChange={onChange}
+                inputValue={inputValue}
+                onInputChange={onInputChange}
+                onKeyPress={onKeyPress}
                 renderInput={params => (
                     <TextField
                         {...params}
-                        error={handleError(inputValue, validation)}     
+                        error={handleError(inputValue, validation)}
                         helperText={inputValue ? "" : handleHelperText(validation)}
-                        InputLabelProps={{ shrink: true }}                        
-                        label={ title }
+                        InputLabelProps={{ shrink: true }}
+                        label={title}
                         InputProps={{
-                        ...params.InputProps,
-                        startAdornment: (
-                            <React.Fragment>
-                                {loadingListOptions ? (
-                                    <CircularProgress color="inherit" size={15} />
-                                ) : null}
-                                {params.InputProps.startAdornment}
-                            </React.Fragment>
-                        )}}
+                            ...params.InputProps,
+                            startAdornment: (
+                                <React.Fragment>
+                                    {loadingListOptions ? (
+                                        <CircularProgress color="inherit" size={15} />
+                                    ) : null}
+                                    {params.InputProps.startAdornment}
+                                </React.Fragment>
+                            )
+                        }}
+                        {...other}
                     />
                 )}
             />
         </React.Fragment>
-           
+
     )
 }
 
 
 CustomInputSelect.propTypes = {
-    title: PropTypes.string, 
-    options: PropTypes.arrayOf(PropTypes.any), 
+    title: PropTypes.string,
+    options: PropTypes.arrayOf(PropTypes.any),
     freeSolo: PropTypes.bool,
     open: PropTypes.bool,
-    value: PropTypes.object, 
-    onChange: PropTypes.func, 
-    inputValue: PropTypes.object, 
-    onInputChange: PropTypes.func, 
-    onKeyPress: PropTypes.func, 
-    onblur: PropTypes.func, 
+    value: PropTypes.object,
+    onChange: PropTypes.func,
+    inputValue: PropTypes.object,
+    onInputChange: PropTypes.func,
+    onKeyPress: PropTypes.func,
+    onblur: PropTypes.func,
     validation: PropTypes.bool,
     disabled: PropTypes.bool,
     loadingListOptions: PropTypes.bool
 };
-  
+
 CustomInputSelect.defaultProp = {
-    title: "", 
+    title: "",
     options: [],
-    freeSolo: false, 
+    freeSolo: false,
     open: false,
-    value: {}, 
-    onChange: ()=>{}, 
-    inputValue: {}, 
-    onInputChange: ()=>{}, 
-    onKeyPress: ()=>{}, 
-    onblur: ()=>{}, 
+    value: {},
+    onChange: () => { },
+    inputValue: {},
+    onInputChange: () => { },
+    onKeyPress: () => { },
+    onblur: () => { },
     validation: false,
     disabled: false,
     loadingListOptions: false
