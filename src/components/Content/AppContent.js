cf98364326f86a33b4dcebf769590af012b3d039
Giovane Sedano <35101491+giovaness30@users.noreply.github.com>
2022-10-05 09:27:10 -0300

    fix: AppContent Component margin

--- src/components/Content/AppContent.js
+++ src/components/Content/AppContent.js
@@ -1,32 +1,32 @@
 import React from "react";
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
-import  PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-const style={
-  appcontent:{      
+const style = {
+  appcontent: {
     transition: '0.4s',
     animation: 'fadeInAnimation ease 0.5s',
     animationIterationCount: 1,
     animationFillMode: 'forwards',
-    marginRight: 'auto',
+    marginRight: '15px',
     marginLeft: 'auto',
     marginTop: '80px',
     maxWidth: '1195px'
-  
+
   }
 }
 
-const AppContent = (props) =>{
+const AppContent = (props) => {
 
-    return(
-      <div style={style.appcontent}>      
-        <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={props.loading}>
-          <CircularProgress color="inherit" />                
-        </Backdrop>
-          { props.children }
-      </div>
-    )
+  return (
+    <div style={style.appcontent}>
+      <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={props.loading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      {props.children}
+    </div>
+  )
 }
 
 
@@ -38,4 +38,4 @@ AppContent.defaultProp = {
   loading: false
 };
 
- export default React.memo(AppContent)
+export default React.memo(AppContent)
