29bacc708b0e1e2bd7a7fbf29ce017fc6577fc56
MatheusGooier <a.matheussouza@gmail.com>
2023-01-31 17:21:44 -0300

    first commit storybook

---
+++ src/storybook/stories/button.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+
+export default {
+  title: 'Button',
+  component: Button
+}
+
+export const text = () => <Button variant='text'>Button</Button>
+export const contained = () => <Button variant='contained'>Button</Button>
+export const outlined = () => <Button variant='outlined'>Button</Button>
